b01f250df3a77e3ddbe9f84a862f0e0e36ac062f
Prajwal Raut <rautprajwal546@gmail.com>
2023-12-07 13:03:57 +0530

    added

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash, compare } from "bcrypt";
+import bcrypt from "bcryptjs";
 import { createToken } from "../utils/token-manager.js";
 import { COOKIE_NAME } from "../utils/constants.js";
 
@@ -29,7 +29,7 @@ export const userSignup = async (
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(401).send("User already registered");
-    const hashedPassword = await hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
@@ -75,7 +75,7 @@ export const userLogin = async (
     if (!user) {
       return res.status(401).send("User not registered");
     }
-    const isPasswordCorrect = await compare(password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
       return res.status(403).send("Incorrect Password");
     }
