e374cedff54bb90f805e538c1e81d3fdd7be3098
Prajwal Raut <rautprajwal546@gmail.com>
2023-12-07 10:08:39 +0530

    cors support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,12 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(cors({
+    origin: 'https://ai-chat-bot-frontend-rautprajwal546-gmailcom.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+  
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
