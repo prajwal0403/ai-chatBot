599c137381657121d3987db516e8372efbc5677b
Prajwal Raut <rautprajwal546@gmail.com>
2023-12-07 13:18:36 +0530

    added dist

---
+++ backend/dist/index.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//connections and listeneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log("Server Open & Connected To Database 🤟");
+    });
+})
+    .catch((err) => {
+    console.error("Error connecting to the database:", err);
+    process.exit(1); // Exit the process if database connection fails
+});
+//# sourceMappingURL=index.js.map
